0d5020f0a57d3d260d0bc5c387763b9c99e927f2
Pedro Rosique <rosiquemurciapedro@gmail.com>
2024-10-24 16:45:23 +0200

    countdown and toast implemented

--- src/index.js
+++ src/index.js
@@ -7,16 +7,33 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+const button = document.querySelector("#start-btn")
 
+button.onclick = () => {
+  startCountdown()
+}
 
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
-
   // Your code goes here ...
+  counterValue = 10
+
+  let intervalId = setInterval(() => {
+    counterValue--
+    document.querySelector('#time').innerText = counterValue
+
+    const startBtn = document.querySelector('#start-btn')
+    startBtn.setAttribute("disabled", "true")
+
+    if (counterValue === 0) {
+      clearInterval(intervalId)
+      showToast()
+    }
+
+
+  }, 1000)
 }
 
 
@@ -24,9 +41,13 @@ function startCountdown() {
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
-
   // Your code goes here ...
+  const toast = document.querySelector('#toast')
+  toast.classList.add("show")
+
+  const timeoutId = setTimeout(() => {
+    toast.classList.remove("show")
+  }, 3000)
 
 
 
@@ -34,5 +55,10 @@ function showToast(message) {
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
+  const button = document.querySelector("#close-toast")
 
+  button.onclick = () => {
+    const toast = document.querySelector('#toast')
+    toast.classList.remove("show")
+  }
 }
